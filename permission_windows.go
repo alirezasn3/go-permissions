a2e7e27f5f9fb6e962c92efed6f261cd08df0441
Alireza Saadatnia <alirezasn3@gmail.com>
2024-10-16 21:37:47 +0330

    added empty functions to avoid build errors on windows and darwin

--- permission_windows.go
+++ permission_windows.go
@@ -1,1 +1,12 @@
 package goPermissions
+
+type Permissions struct {
+	Read    bool
+	Write   bool
+	Execute bool
+}
+
+// This function only works on linux
+func GetPermissions(path string) (*Permissions, error) {
+	return &Permissions{}, nil
+}
